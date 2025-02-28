e79badb548bfdbfac20f29d281742f55b58fed03
armen <armenbakir@hotmail.com>
2025-02-28 10:03:10 +0100

    added route for speech

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: "Missing API key" }, { status: 500 });
   }
 
-  const response = await axios.post(
+  const chatResponse = await axios.post(
     "https://api.openai.com/v1/chat/completions",
     {
       model: "gpt-3.5-turbo",
@@ -23,5 +23,36 @@ export async function POST(request: NextRequest) {
       },
     }
   );
-  return NextResponse.json(response.data);
+
+  const aiTextResponse = chatResponse.data.choices[0].message.content.trim();
+
+  const ttsResponse = await axios.post(
+    "https://api.openai.com/v1/audio/speech",
+    {
+      model: "tts-1",
+      input: aiTextResponse,
+      voice: "alloy",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      responseType: "arraybuffer",
+    }
+  );
+
+  return new NextResponse(
+    JSON.stringify({
+      text: aiTextResponse,
+      audio: Buffer.from(ttsResponse.data).toString("base64"),
+    }),
+
+    {
+      status: 200,
+      headers: {
+        "Conent-Type": "application/json",
+      },
+    }
+  );
 }
